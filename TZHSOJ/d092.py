899a503a5149d0a5cf6944124c62018480d58d1f
陆田所 <127110559+lmy114@users.noreply.github.com>
2023-03-10 20:49:06 +0800

    Create d092.py
    lmy

---
+++ TZHSOJ/d092.py
@@ -0,0 +1,15 @@
+def jc(a):
+    if a==1:
+        return 1
+    elif a==0:
+        return 1
+    else:
+        return a*jc(a-1)
+a,b,c,d=input().split()
+a=int(a)
+b=int(b)
+c=int(c)
+d=int(d)
+c1=jc(a)/(jc(b)*jc(a-b))
+c2=jc(c)/(jc(d)*jc(c-d))
+print(int(c1)+int(c2))
