7d167299985b7dcc5dca2cf8cc644cefe49e1d53
Florance <127110459+FloranceYeh@users.noreply.github.com>
2023-03-10 13:05:53 +0800

    Create d050.py

---
+++ TZHSOJ/d050.py
@@ -0,0 +1,13 @@
+a, b, c = input().split()
+s = input()
+a = int(a)
+b = int(b)
+c = int(c)
+for i in range(0, a):
+    print(s, end='')
+print()
+for i in range(0, b):
+    print(s, end='')
+print()
+for i in range(0, c):
+    print(s, end='')
