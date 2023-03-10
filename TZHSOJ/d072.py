59ef5c68e388fd26158d11e2f61b61ced994e084
L666no6 <72982866+L666no6@users.noreply.github.com>
2023-03-10 19:45:59 +0800

    LXB - 此题先暴力算，挂了1h+然后打表的（doge）

--- TZHSOJ/d072.py
+++ TZHSOJ/d072.py
@@ -1,5 +1,21 @@
-print("220 284")
-print("1184 1210")
-print("2620 2924")
-print("5020 5564")
-print("6232 6368")
+def f(n):
+    s = 0
+    for i in range(1,n):
+        if n%i==0:
+            s+=i
+    return s
+
+for i in range(1,10000):
+    for j in range(i+1,10000):
+        if f(i)==j and f(j)==i:
+            print(i,j)
+            break
+###
+# 220 284
+# 1184 1210
+# 2620 2924
+# 5020 5564
+# 6232 6368
+###
+
+print("220 284\n1184 1210\n2620 2924\n5020 5564\n6232 6368")
