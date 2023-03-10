d54c5dfbfa2e444b600ee576dce3aa7e0229cf73
Florance <127110459+FloranceYeh@users.noreply.github.com>
2023-03-10 18:44:59 +0800

    Update d054.py

--- TZHSOJ/d054.py
+++ TZHSOJ/d054.py
@@ -1,3 +1,1 @@
-print("a:Sat\nb:Sun\nc:Fri\nd:Wed\ne:Mon\nf:Thu\ng:Tue")
- # print("a : Sat\nb : Sun\nc : Fri\nd : Wed\ne : Mon\nf : Thu\ng : Tue")
- # 旧OJ输出冒号两边有空格！
+print("a : Sat\nb : Sun\nc : Fri\nd : Wed\ne : Mon\nf : Thu\ng : Tue")
