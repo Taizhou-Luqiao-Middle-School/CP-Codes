79f346c61e0c992e37661c9d2ee71e7edc5bef26
L666no6 <72982866+L666no6@users.noreply.github.com>
2023-03-10 15:16:39 +0800

    LXB

--- TZHSOJ/d061.py
+++ TZHSOJ/d061.py
@@ -1,11 +1,10 @@
-n=int(input())
-a=int((n*(n-1))/2+1)
-print(a)
-for i in range(2,n+1,1):
-    a=a-(n-i+1)
-    print(a,end=" ")
-    b=a
-    for j in range(2,i+1,1):
-        b=b+(n-i+j)
-        print(b,end=" ")
-    print(" ")
+n = int(input())
+for i in range(n+1,1,-1):
+    s=""
+    d = i
+    now = int( (i-2)*(i-1)/2 )+1
+    for j in range(0,n+2-i,1):
+        s = s + str(now) + ' '
+        now += d
+        d += 1
+    print(s)
