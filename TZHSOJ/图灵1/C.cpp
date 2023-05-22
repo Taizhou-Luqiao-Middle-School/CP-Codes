58c253cfedb5af0a48a7c737eebc026dd6a1408e
L666no6 <72982866+L666no6@users.noreply.github.com>
2023-05-22 04:48:54 +0000

    图灵1update

---
+++ TZHSOJ/图灵1/C.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+signed main(){
+    int p=0;
+    cin>>p;//刷掉输入
+    string input;
+    cin>>input;
+    char sign = input[0];
+    p=0;
+    int len[1000]={};
+    for (int i=0;i<input.length();i++){
+        if (sign==input[i]){
+            len[p]++;
+        }
+        else {
+            p++;
+            sign=input[i];
+            len[p]++;
+        }
+    }
+    int sum=0;
+    for (int i=0;i<=p;i++){
+        len[i] = len[i] % 2;
+    }
+    
+    for (int i=0;i<=p;i++){
+        if (len[i]==0){
+            continue;
+        }
+        int k=i+1;
+        for (k=i+1;k<=p;k++){
+            if (len[k]==1){
+                break;
+            }
+            
+        }
+        sum += k-i;
+        i=k;
+    }
+    
+    cout<<sum;
+    
+    return 0;
+}
