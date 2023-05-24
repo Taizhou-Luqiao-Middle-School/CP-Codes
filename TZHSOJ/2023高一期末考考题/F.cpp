bed13656e6218612b18db145c8ecb6fd558fd83d
L666no6 <72982866+L666no6@users.noreply.github.com>
2023-05-24 08:38:41 +0000

    F

---
+++ TZHSOJ/2023高一期末考考题/F.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+signed main(){
+    string s;
+    cin>>s;
+    if (s[0]=='A'){
+        cout<<"Hangzhou";
+    }
+    else if (s[0]=='B'){
+        cout<<"Ningbo";
+    }
+    else if (s[0]=='C'){
+        cout<<"Wenzhou";
+    }
+    else if (s[0]=='D'){
+        cout<<"Shaoxing";
+    }
+    else if (s[0]=='E'){
+        cout<<"Huzhou";
+    }
+    else if (s[0]=='F'){
+        cout<<"Jiaxing";
+    }
+    else if (s[0]=='G'){
+        cout<<"Jinhua";
+    }
+    else if (s[0]=='H'){
+        cout<<"Quzhou";
+    }
+    else if (s[0]=='J'){
+        cout<<"Taizhou";
+    }
+    else if (s[0]=='L'){
+        cout<<"Lishui";
+    }
+    else if (s[0]=='K'){
+        cout<<"Zhoushan";
+    }
+    else {
+        cout<<"Input error";
+    }
+    return 0;
+}
