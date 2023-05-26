4477bb4ddcdcd5fdf9f11958a3e4cfc2dec5ec9e
L666no6 <72982866+L666no6@users.noreply.github.com>
2023-05-26 16:44:39 +0800

    给D添加注释

--- TZHSOJ/2023高一期末考考题/D.py
+++ TZHSOJ/2023高一期末考考题/D.py
@@ -1,8 +1,9 @@
 l=[]
 a=input()
 for i in a:
-    l.insert(0,i)
+    l.insert(0,i)#如何使用insert() : https://www.runoob.com/python3/python3-att-list-insert.html
+    #含义是把i添加到列表的第一位
 
-l.sort(reverse=True)
+l.sort(reverse=True) # python 自带的排序函数
 for i in l:
 print(i,end='')
