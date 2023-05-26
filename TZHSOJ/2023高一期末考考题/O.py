5af9fd00e7786cc6d110671cec1f8a2027aa8161
L666no6 <72982866+L666no6@users.noreply.github.com>
2023-05-26 17:45:05 +0800

    O 注释加了，但是没有完全加（

--- TZHSOJ/2023高一期末考考题/O.py
+++ TZHSOJ/2023高一期末考考题/O.py
@@ -5,6 +5,9 @@ num_cases = int(input().strip())
 for i in range(num_cases):
     password = input().strip()
     if re.match(r'(?=.*[A-Z])(?=.*[a-z])(?=.*\d|.*[!@#$%^&*()~-]).{8,16}$', password):
+        # 补充： re.match  https://blog.csdn.net/qq_33210042/article/details/116794784
+        # 括号内的是要找的字符
+        # 但是压行压成这p样我也不是很清楚其中的原理了
         print('YES')
     else:
         print('NO')
