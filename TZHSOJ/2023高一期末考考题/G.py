a9830c4ce7264bfd1a016273873712991645afaa
L666no6 <72982866+L666no6@users.noreply.github.com>
2023-05-26 16:48:14 +0800

    给G加注释（

--- TZHSOJ/2023高一期末考考题/G.py
+++ TZHSOJ/2023高一期末考考题/G.py
@@ -1,6 +1,10 @@
 l=input()
 c=int(len(l))
 if(l[0:int(c/2)]==l[-1:int(-c/2-1):-1]):
+    # l[0:int(c/2)] 的含义是截取l列表的前一半
+    # l[-1:int(-c/2-1):1] 的含义是截取l列表的后一半
+    # 本句是在比较 l 的前一半和后一半是否相等
+    # https://www.runoob.com/python3/python3-list.html
     print("Yes")
 else :
     print("No")
