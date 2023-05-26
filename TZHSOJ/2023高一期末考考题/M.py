d5e05d0a99ffc19107f5f3836830ea678234fab8
L666no6 <72982866+L666no6@users.noreply.github.com>
2023-05-26 16:39:48 +0800

    给M题加注释

--- TZHSOJ/2023高一期末考考题/M.py
+++ TZHSOJ/2023高一期末考考题/M.py
@@ -5,7 +5,9 @@ n = int(input().strip())
 def is_prime(x):
     if x < 2:
         return False
-    for i in range(2, int(math.sqrt(x))+1):
+    for i in range(2, int(math.sqrt(x))+1):#如果你内存超限了，应该是这一行没有做好优化
+        #枚举因数的时候只要枚举到 根号x 就可以了
+        #用 int(x**0.5)+1 表示也可
         if x % i == 0:
             return False
     return True
